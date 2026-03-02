dcc75451ad249a7f2fe32a9d6ee56465f2368c48
Mandeep Singh <smand@Mandeep-PC>
2026-03-02 12:20:22 +0530

    Grafna Use

--- product-service/src/main/java/com/example/microservice/product/service/ProductService.java
+++ product-service/src/main/java/com/example/microservice/product/service/ProductService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+
 public class ProductService {
 
     @Autowired
@@ -31,7 +32,6 @@ public class ProductService {
         product.setPrice(productRequest.getPrice());
         product.setDescription(productRequest.getDescription());
         productRepo.save(product);
-
         log.info("Product is saved ");
 
         return   new ProductResponse(product.getId(), product.getName(), product.getDescription(),product.getPrice());
